49f68d1d175d9a976fcf31dcd848ea85fd1e6c06
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-12-30 17:59:40 +0530

    chore: updated the auth.config.ts for production readyness

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,14 @@
+const data =
+  process.env.NODE_ENV === 'development'
+    ? {
+        domain: 'https://premium-griffon-43.clerk.accounts.dev/',
+        applicationID: 'convex',
+      }
+    : {
+        domain: 'https://clerk.fsadp.com',
+        applicationID: 'convex',
+      };
+
 export default {
-  providers: [
-    {
-      domain: "https://premium-griffon-43.clerk.accounts.dev/",
-      applicationID: "convex",
-    },
-  ],
+  providers: [data],
 };
